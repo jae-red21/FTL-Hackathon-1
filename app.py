d0ebc8091fe2f87eb62474277060933a72e488f5
Mussie <icemussie@gmail.com>
2025-03-14 11:39:21 +0300

    Backend setup

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api
+import os
+
 
 app = Flask(__name__)
+
+# Configure SQLite database
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASE_DIR, "database/electricity.db")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Initialize API and Database
+db = SQLAlchemy(app)
+api = Api(app)
+
 
 @app.route("/")
 def index():
